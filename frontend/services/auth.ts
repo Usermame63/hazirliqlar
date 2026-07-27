be2f3704787078d4fba4b937dd292d08975cd023
qaqas123 <zelgun730@gmail.com>
2026-07-28 00:47:55 +0400

    backend linki yenilendi

--- frontend/services/auth.ts
+++ frontend/services/auth.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const registerUser = async (data: any) => {
-  const res = await axios.post("http://localhost:5000/api/auth/register", data);
+  const res = await axios.post("https://hazirliqlar-backend.onrender.com/api/auth/register", data);
   return res.data;
 };
 
 // YENİ: Login artıq yalnız Mail və Şifrə alır, OTP_SENT cavabı verir
 export const loginUser = async (email: string, password: string) => {
-  const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+  const res = await axios.post("https://hazirliqlar-backend.onrender.com/api/auth/login", { email, password });
   return res.data;
 };
 
 // YENİ: Gələn kodu yoxlayan API
 export const verifyOtp = async (email: string, otp: string) => {
-  const res = await axios.post("http://localhost:5000/api/auth/verify-otp", { email, otp });
+  const res = await axios.post("https://hazirliqlar-backend.onrender.com/api/auth/verify-otp", { email, otp });
   return res.data;
 };
