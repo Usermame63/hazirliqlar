293b92976a853af679278e29fb19fcf29578a1ab
qaqas123 <zelgun730@gmail.com>
2026-08-05 02:36:56 +0400

    Duzeldilmis next-intl config tipi

--- frontend/next-intl.config.ts
+++ frontend/next-intl.config.ts
@@ -1,9 +1,9 @@
 import { IntlConfig } from 'next-intl';
 
-const config: NextIntlConfig = {
+const config: IntlConfig = {
   locales: ['az', 'en', 'tr', 'ru'],
   defaultLocale: 'az',
-  localeDetection: false, // Statik exportda dil avtomatik təyinini söndürürük
+  localeDetection: false,
 };
 
 export default config;
